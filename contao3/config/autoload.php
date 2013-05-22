ef83896a2c1b32ffbdff91f7a0836652cdda7f5c
Tim Gatzky <info@tim-gatzky.de>
2013-05-22 08:24:10 +0200

    added: moo_onepagewebsitenavigation in templates

--- contao3/config/autoload.php
+++ contao3/config/autoload.php
@@ -6,6 +6,8 @@
  * Copyright (C) 2005-2012 Leo Feyer
  * 
  * @package OnePageWebsite
+ * @copyright	Tim Gatzky 2013
+ * @author		Tim Gatzky <info@tim-gatzky.de>
  * @link    http://contao.org
  * @license http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
@@ -34,4 +36,5 @@ TemplateLoader::addFiles(array
 	'mod_onepage'         	=> 'system/modules/OnePageWebsite/templates',
 	'moo_smoothScroll' 		=> 'system/modules/OnePageWebsite/templates',
 	'opw_default'     		=> 'system/modules/OnePageWebsite/templates',
+	'moo_onepagewebsitenavigation' => 'system/modules/OnePageWebsite/templates',
 ));
