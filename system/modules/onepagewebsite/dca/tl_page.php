1b2378f926070aacc87ddcbe3d680ac068c9e140
Tim Gatzky <info@tim-gatzky.de>
2013-11-23 14:09:02 +0100

    updated header

--- system/modules/onepagewebsite/dca/tl_page.php
+++ system/modules/onepagewebsite/dca/tl_page.php
@@ -2,30 +2,14 @@
 
 /**
  * Contao Open Source CMS
- * Copyright (C) 2005-2010 Leo Feyer
- *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
+ * Copyright (C) 2005-2013 Leo Feyer
  * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Tim Gatzky 2012 
- * @author     Tim Gatzky <info@tim-gatzky.de>
- * @package    OnePageWebsite
- * @license    LGPL 
- * @filesource
+ * @copyright	Tim Gatzky 2013
+ * @author		Tim Gatzky <info@tim-gatzky.de>
+ * @package		OnePageWebsite
+ * @link		http://contao.org
+ * @license		http://www.gnu.org/licenses/lgpl-3.0.html LGPL
  */
 
 /**
